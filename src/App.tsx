e366a6e4ccab7f0c0eefd3cd083ace303b083881
kostenko svyatoslav <Sladrigezz@github.com>
2022-06-29 13:21:31 +0300

    add timer and game restart

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
 import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
 import { Board } from "./models/Board";
 import { Colors } from "./models/Colors";
 import { Player } from "./models/Player";
 
 function App() {
   const [board, setBoard] = useState(new Board());
-  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
-  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [whitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer] = useState(new Player(Colors.BLACK));
   const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
@@ -28,10 +29,12 @@ function App() {
     newBoard.initCells();
     newBoard.addFigures();
     setBoard(newBoard);
+    setCurrentPlayer(whitePlayer)
   }
 
   return (
     <div className="app">
+      <Timer currentPlayer={currentPlayer} restart={restart} />
       <BoardComponent
         board={board}
         setBoard={setBoard}
